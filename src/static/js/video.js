0620a665306ef208681d686699e0cd6bdea77627
Parikshit Jagtap <43935824+itzparikshit@users.noreply.github.com>
2018-10-18 00:36:27 +0530

    Update video.js

--- src/static/js/video.js
+++ src/static/js/video.js
@@ -25,7 +25,7 @@ function enableVideo() {
 // kickstart the video call.
 function loadSimpleWebRTC() {
   var script = document.createElement("script");
-  script.src = "https://github.com/andyet/SimpleWebRTC/blob/master/src/webrtc.js";
+  script.src = "https://github.com/andyet/SimpleWebRTC/blob/master/src/simplewebrtc.js";
   document.head.appendChild(script);
 
   script.onload = function() {
