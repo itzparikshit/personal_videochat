d8e28eedbcc6304cc5784d36d16a07fb079210eb
Parikshit Jagtap <43935824+itzparikshit@users.noreply.github.com>
2018-10-18 00:32:37 +0530

    Update video.js

--- src/static/js/video.js
+++ src/static/js/video.js
@@ -25,7 +25,7 @@ function enableVideo() {
 // kickstart the video call.
 function loadSimpleWebRTC() {
   var script = document.createElement("script");
-  script.src = "https://simplewebrtc.com/latest-v3.js";
+  script.src = "https://github.com/andyet/SimpleWebRTC/blob/master/src/webrtc.js";
   document.head.appendChild(script);
 
   script.onload = function() {
